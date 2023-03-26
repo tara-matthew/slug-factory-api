6045e2aaf6a8b8425b944848fb8804a9341b60bf
Tara Matthew <tara@tutorful.co.uk>
2023-03-26 17:01:37 +0100

    Amend Storybook main.js

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -2,12 +2,14 @@
 const config = {
   "stories": [
     "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../components/**/*.stories.mdx",
+    "../components/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
   "framework": {
     "name": "@storybook/vue3-vite",
