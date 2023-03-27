5a89531ca214cf2e2110dfb1b4e81cfb840aed32
Tara Matthew <tara@tutorful.co.uk>
2023-03-27 22:17:53 +0100

    Style inputs

--- frontend/components/atom/BaseInput.vue
+++ frontend/components/atom/BaseInput.vue
@@ -1,3 +1,3 @@
 <template>
-    <input v-bind="$attrs" class="border rounded-md border-purple-800" />
+    <input v-bind="$attrs" class="border rounded-md border-purple-800 focus:outline-purple-900" />
 </template>
