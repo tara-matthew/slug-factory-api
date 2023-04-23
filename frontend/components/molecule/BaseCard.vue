c723f1ee905d85479a013fd515de0dff55fae1ae
Tara Matthew <tara@tutorful.co.uk>
2023-04-23 22:58:47 +0100

    Begin Card molecule

---
+++ frontend/components/molecule/BaseCard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="bg-white rounded-lg overflow-hidden shadow-md">
+        <div class="px-4 py-2">
+            <div class="mb-3">
+                <slot name="image" />
+            </div>
+            <div class="text-lg font-bold mb-2">
+                <slot name="title" />
+            </div>
+            <slot name="content" />
+        </div>
+        <div class="px-4 py-2 bg-gray-100">
+            <slot name="footer" />
+        </div>
+    </div>
+</template>
