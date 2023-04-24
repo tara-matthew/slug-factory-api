27831c95b3fa62909a3157d839bd44508312d5cb
Tara Matthew <tara@tutorful.co.uk>
2023-04-24 21:50:07 +0100

    Begin BaseGrid and test out

---
+++ frontend/components/organism/BaseGrid.vue
@@ -0,0 +1,18 @@
+<template>
+    <div :class="gridClasses" class="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    columns: {
+        type: Number,
+        default: 4
+    }
+});
+
+const gridClasses = computed(() => {
+    return `grid-cols-${props.columns}`; // TODO some responsive classes with different cols depending on screen size
+});
+</script>
