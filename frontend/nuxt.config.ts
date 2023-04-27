f5827437ed1897f1f419b6e1c93ad9259c9e808b
Tara Matthew <tara@tutorful.co.uk>
2023-04-27 19:37:23 +0100

    Install NuxtLink

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
 export default defineNuxtConfig({
+    modules: [
+        "@nuxt/image-edge",
+    ],
     css: ["~/assets/css/main.css"],
     postcss: {
         plugins: {
