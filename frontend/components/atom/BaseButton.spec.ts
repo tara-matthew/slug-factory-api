fc475e755fffb3c8dde6c7821b4c1062d67189c4
Tara Matthew <tara@tutorful.co.uk>
2023-05-02 17:20:10 +0100

    Add Vitest, first test file, blank vue page to stop test warnings

---
+++ frontend/components/atom/BaseButton.spec.ts
@@ -0,0 +1,43 @@
+// @vitest-environment nuxt
+import { describe, it, expect } from "vitest";
+import { mount } from "@vue/test-utils";
+import BaseButton from "../atom/BaseButton.vue";
+
+describe("Base Button text", () => {
+    it("displays the correct text", () => {
+        const wrapper = mount(BaseButton, {
+            props: {
+                text: "My button",
+                componentType: "link"
+            }
+        });
+
+        expect(wrapper.text()).toContain("My button");
+    });
+});
+
+// TODO may be able to streamline this into one test rather than having two repetitive tests
+describe("Component type", () => {
+    it("is a link when link is passed in as a prop", () => {
+        // console.log(BaseButton)
+        const wrapper = mount(BaseButton, {
+            props: {
+                text: "hello",
+                componentType: "link"
+            }
+        });
+
+        expect(wrapper.find("a").exists()).toBe(true); // TODO can we check for an actual NuxtLink (not essential)
+    });
+    it("is a button when button is passed in as a prop", () => {
+        // console.log(BaseButton)
+        const wrapper = mount(BaseButton, {
+            props: {
+                text: "hello",
+                componentType: "button"
+            }
+        });
+
+        expect(wrapper.find("button").exists()).toBe(true);
+    });
+});
