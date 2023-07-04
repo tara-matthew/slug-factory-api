deb053127703456ea570ed582f18f011f5b022fb
Tara Matthew <tara@tutorful.co.uk>
2023-07-04 18:28:24 +0100

    Fix todos

--- frontend/components/organism/NavBar.vue
+++ frontend/components/organism/NavBar.vue
@@ -1,14 +1,16 @@
 <template>
     <nav class="p-5 flex justify-between bg-purple-500 items-center">
+        <!-- TODO change to a logo atom at some point -->
         <div>
-            <a class="text-white font-bold" href="#" style="font-size: 18px;">Slug Factory</a>
+            <NuxtLink class="text-white font-bold" to="/home" style="font-size: 18px;">Slug Factory</NuxtLink>
         </div>
         <ul class="flex items-center">
             <li v-for="(link, index) in links" :key="index">
-                <a href="#" class="pr-8 text-white">{{ link.title }}</a>
+                <NuxtLink :to="link.path" class="pr-8 text-white">{{ link.title }}</NuxtLink>
             </li>
+            <!-- TODO this will be dynamic for the logged in user -->
             <li>
-                <a href="#" class="pl-7 text-white font-bold" style="font-size: 18px;">Tara</a>
+                <NuxtLink to="#" class="pl-7 text-white font-bold" style="font-size: 18px;">Tara</NuxtLink>
             </li>
         </ul>
     </nav>
