c1b835efa883ffeaf0bc19d63d0266ead1ca4e3f
Tara Matthew <tara@tutorful.co.uk>
2023-09-10 12:34:45 +0100

    Fix test

--- backend/tests/Unit/FormRequest/StorePrintedDesignRequestTest.php
+++ backend/tests/Unit/FormRequest/StorePrintedDesignRequestTest.php
@@ -30,7 +30,10 @@ class StorePrintedDesignRequestTest extends TestCase
             [
             'title' => 'required|max:255',
             'description' => 'required',
-            'user_id' => 'required|exists:users,id'
+            'user_id' => 'required|exists:users,id',
+            'filament_brand_id' => 'required',
+            'filament_colour_id' => 'required',
+            'images.*.url' => 'required'
         ],
             $this->printedDesignRequest->rules()
         );
