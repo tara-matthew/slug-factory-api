569551a4991468f7fa2db750035d156beac390d3
Tara Matthew <tara@tutorful.co.uk>
2023-09-18 21:18:43 +0100

    Amend logout route

--- backend/app/Http/Controllers/LogoutController.php
+++ backend/app/Http/Controllers/LogoutController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 
 class LogoutController extends Controller
 {
-    public function __invoke(): void
+    public function __invoke(): Response
     {
-        Auth::logout();
+        Auth::guard('web')->logout();
+
+        return response()->noContent();
     }
 }
