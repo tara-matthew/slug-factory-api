75b3f8bda6b36875d78f21b62d8a0a715ba83427
Tara Matthew <tara@tutorful.co.uk>
2024-02-24 16:23:30 +0000

    Add Filament resource for printer filament brands

---
+++ src/App/Filament/Resources/FilamentBrandResource.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\FilamentBrandResource\Pages;
+use App\Filament\Resources\FilamentBrandResource\RelationManagers;
+use Domain\Filaments\Brands\Models\FilamentBrand;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class FilamentBrandResource extends Resource
+{
+    protected static ?string $model = FilamentBrand::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListFilamentBrands::route('/'),
+            'create' => Pages\CreateFilamentBrand::route('/create'),
+            'edit' => Pages\EditFilamentBrand::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ src/App/Filament/Resources/FilamentBrandResource/Pages/CreateFilamentBrand.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\FilamentBrandResource\Pages;
+
+use App\Filament\Resources\FilamentBrandResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateFilamentBrand extends CreateRecord
+{
+    protected static string $resource = FilamentBrandResource::class;
+}
