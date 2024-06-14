f0702037e6d52751e83376023751c77a52f0d04b
tara-matthew <taramatthew.93@gmail.com>
2024-06-14 22:03:20 +0100

    Move to Laravel 11 file structure

--- src/App/Providers/AppServiceProvider.php
+++ src/App/Providers/AppServiceProvider.php
@@ -2,31 +2,23 @@
 
 namespace App\Providers;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
-    public function register()
+    /**
+     * Register any application services.
+     */
+    public function register(): void
     {
-        if (! $this->app->environment('production')) {
-            $this->app->register('App\Providers\FakerServiceProvider');
-        }
+        //
     }
 
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
-        Factory::guessFactoryNamesUsing(function (string $modelName) {
-            return '\Database\Factories\\'.class_basename($modelName).'Factory';
-        });
-
-        Password::defaults(function () {
-            return Password::min(8)
-                ->letters()
-                ->mixedCase()
-                ->numbers()
-                ->symbols();
-        });
+        //
     }
 }
