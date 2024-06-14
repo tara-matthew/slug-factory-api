a7f21c3d9861ebef540acc8cc2da2893a8ea29d6
tara-matthew <taramatthew.93@gmail.com>
2024-06-14 22:16:54 +0100

    Add back password defaults

--- src/App/Providers/AppServiceProvider.php
+++ src/App/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,6 +23,14 @@ class AppServiceProvider extends ServiceProvider
     {
         Factory::guessFactoryNamesUsing(function (string $modelName) {
             return '\Database\Factories\\'.class_basename($modelName).'Factory';
+        });
+
+        Password::defaults(function () {
+            return Password::min(8)
+                ->letters()
+                ->mixedCase()
+                ->numbers()
+                ->symbols();
         });
     }
 }
