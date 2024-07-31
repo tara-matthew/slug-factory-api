846e274b94c88bb8823c18d078c897268fa9e97b
tara-matthew <taramatthew.93@gmail.com>
2024-07-31 11:21:12 +0100

    Update routes

--- routes/api.php
+++ routes/api.php
@@ -1,21 +1,41 @@
 <?php
 
 use App\Favourites\Controllers\FavouritePrintedDesignController;
+use App\Favourites\Controllers\IndexMyFavouritePrintedDesignsController;
 use App\Filaments\Brands\Controllers\FilamentBrandController;
 use App\Filaments\Colours\Controllers\FilamentColourController;
-use App\PrintedDesigns\Controllers\IndexPrintedDesignsController;
+use App\PrintedDesigns\Controllers\IndexMyPrintedDesignsController;
+use App\PrintedDesigns\Controllers\LatestPrintedDesignsController;
 use App\PrintedDesigns\Controllers\ShowPrintedDesignController;
 use App\PrintedDesigns\Controllers\StorePrintedDesignController;
+use App\PrintedDesigns\Resources\PrintedDesignResource;
+use Domain\PrintedDesigns\Models\PrintedDesign;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/prints', IndexPrintedDesignsController::class)->name('prints.index');
+//Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/prints/latest', LatestPrintedDesignsController::class)->name('prints.latest.index');
+    Route::get('/prints/random', function () {
+        return PrintedDesignResource::collection(
+            PrintedDesign::query()
+                ->inRandomOrder()
+                ->take(10)
+                ->get()
+        );
+    });
+
+    Route::get('/prints', IndexMyPrintedDesignsController::class)->name('my.prints.index');
     Route::get('/prints/{printedDesign}', ShowPrintedDesignController::class)->name('prints.show');
     Route::post('/prints', StorePrintedDesignController::class)->name('prints.store');
 
     Route::resource('filament-brands', FilamentBrandController::class);
-    Route::resource('users.favourite-printed-designs', FavouritePrintedDesignController::class)->parameters(['favourite-printed-designs' => 'printed_design']);
-});
+//    Route::resource('users.favourite-printed-designs', FavouritePrintedDesignController::class)->parameters(['favourite-printed-designs' => 'printed_design']); // todo change to my
+
+    Route::get('/favourites/printed-designs', IndexMyFavouritePrintedDesignsController::class)->name('my.favourites.printed-designs.index'); // todo this may replace the favourite printed design controller
+
+//    Route::post('/favourites');
+//});
+
+
 
 Route::resource('filament-colours', FilamentColourController::class);
 
