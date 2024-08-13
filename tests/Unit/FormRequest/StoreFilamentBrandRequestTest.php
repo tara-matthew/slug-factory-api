cf7b3163b503388773d3465b1ad9f88c057f0aa5
tara-matthew <taramatthew.93@gmail.com>
2024-08-13 13:25:29 +0100

    Update store tests

--- tests/Unit/FormRequest/StoreFilamentBrandRequestTest.php
+++ tests/Unit/FormRequest/StoreFilamentBrandRequestTest.php
@@ -3,27 +3,18 @@
 namespace Tests\Unit\FormRequest;
 
 use App\Filaments\Brands\Requests\StoreFilamentBrandRequest;
-use PHPUnit\Framework\Attributes\Test;
-use Tests\TestCase;
+use JMac\Testing\Traits\AdditionalAssertions;
 
-class StoreFilamentBrandRequestTest extends TestCase
-{
-    private StoreFilamentBrandRequest $storeFilamentBrandRequest;
+uses(AdditionalAssertions::class);
 
-    protected function setUp(): void
-    {
-        parent::setUp();
-        $this->storeFilamentBrandRequest = new StoreFilamentBrandRequest();
-    }
+it('has rules set up correctly', function () {
+    $storeFilamentBrandRequest = new StoreFilamentBrandRequest();
 
-    #[Test]
-    public function it_has_rules_set_up_as_expected(): void
-    {
-        $this->assertEquals(
-            [
-                'name' => 'required|string|max:255',
-            ],
-            $this->storeFilamentBrandRequest->rules()
-        );
-    }
-}
+    $this->assertEquals(
+        [
+            'name' => 'required|string|max:255',
+        ],
+
+        $storeFilamentBrandRequest->rules()
+    );
+});
--- tests/Unit/FormRequest/StorePrintedDesignRequestTest.php
+++ tests/Unit/FormRequest/StorePrintedDesignRequestTest.php
@@ -4,37 +4,22 @@ namespace Tests\Unit\FormRequest;
 
 use App\PrintedDesigns\Requests\StorePrintedDesignRequest;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use JMac\Testing\Traits\AdditionalAssertions;
-use PHPUnit\Framework\Attributes\Test;
-use Tests\TestCase;
 
-class StorePrintedDesignRequestTest extends TestCase
-{
-    use AdditionalAssertions;
-    use RefreshDatabase;
+uses(RefreshDatabase::class);
 
-    private StorePrintedDesignRequest $printedDesignRequest;
+it('has rules set up correctly', function () {
+    $storePrintedDesignRequest = new StorePrintedDesignRequest();
 
-    protected function setUp(): void
-    {
-        parent::setUp();
-        $this->printedDesignRequest = new StorePrintedDesignRequest();
-    }
-
-    #[Test]
-    public function it_has_rules_set_up_as_expected(): void
-    {
-        $this->assertEquals(
-            [
-                'title' => 'required|max:255',
-                'description' => 'required',
-                'filament_brand_id' => 'required:exists:filament_brands,id',
-                'filament_colour_id' => 'required:exists:filament_colours,id',
-                'filament_material_id' => 'required:exists:filament_materials,id',
-                'images.*.url' => 'required',
-                'images.*.is_cover_image' => 'required|bool',
-            ],
-            $this->printedDesignRequest->rules()
-        );
-    }
-}
+    $this->assertEquals(
+        [
+            'title' => 'required|max:255',
+            'description' => 'required',
+            'filament_brand_id' => 'required:exists:filament_brands,id',
+            'filament_colour_id' => 'required:exists:filament_colours,id',
+            'filament_material_id' => 'required:exists:filament_materials,id',
+            'images.*.url' => 'required',
+            'images.*.is_cover_image' => 'required|bool',
+        ],
+        $storePrintedDesignRequest->rules()
+    );
+});
