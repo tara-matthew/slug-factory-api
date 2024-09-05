6c31019940389f7f11a04bcb2ea9c2b40f9cb402
tara-matthew <taramatthew.93@gmail.com>
2024-09-05 18:37:03 +0100

    Add tests for unfavouriting, throw an exception

--- src/App/Favourites/Controllers/DeleteFavouriteController.php
+++ src/App/Favourites/Controllers/DeleteFavouriteController.php
@@ -2,23 +2,27 @@
 
 namespace App\Favourites\Controllers;
 
+use App\Exceptions\ItemNotFavouritedException;
 use Domain\Shared\Traits\IdentifiesModels;
-use Domain\Users\Models\User;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 class DeleteFavouriteController
 {
     use IdentifiesModels;
 
+    /**
+     * @throws ItemNotFavouritedException
+     */
     public function __invoke(string $type, string $id): Response
     {
-        $user = User::find(1);
+        $user = Auth::user();
         $model = $this->identifyModel($type, $id);
 
         if (! $model->isFavourite()) {
-            // throw an exception
+            throw new ItemNotFavouritedException();
         }
-        $model->favourites()->where('user_id', $user->id)->delete();
+        $model->favourites()->where('user_id', $user->id)->delete(); // wherebelongsto?
 
         return response()->noContent();
 
---
+++ tests/Feature/Favourites/RemoveFavouriteTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use Domain\Favourites\Models\Favourite;
+use Domain\PrintedDesigns\Models\PrintedDesign;
+use Domain\Users\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\Fluent\AssertableJson;
+
+uses(RefreshDatabase::class);
+
+it('deletes a favourite', function () {
+    $user = User::factory()->create();
+
+    $favouritedPrint = PrintedDesign::factory()
+        ->for($user)
+        ->create();
+
+    Favourite::factory()->for(
+        $favouritedPrint, 'favouritable'
+    )->for($user)->create();
+
+    $this->assertCount(1, Favourite::all());
+
+    asLoggedInUser()
+        ->delete(route('favourites.delete', ['type' => 'printed_design', 'id' => $favouritedPrint->id]))
+        ->assertNoContent();
+
+    $this->assertCount(0, Favourite::all());
+});
+
+it('throws an exception if an attempt is made to unfavourite an item which has not been favourited', function() {
+    $user = User::factory()->create();
+
+    $print = PrintedDesign::factory()
+        ->for($user)
+        ->create();
+
+    asLoggedInUser()
+        ->delete(route('favourites.delete', ['type' => 'printed_design', 'id' => $print->id]))
+        ->assertUnprocessable()
+        ->assertJson(fn (AssertableJson $json) => $json
+            ->where('message', 'Item has not been added to favourites'));;
+});
