19473de930cd97afbd5fc4cf279c7b7bb5e35c3d
tara-matthew <taramatthew.93@gmail.com>
2024-09-09 23:55:28 +0100

    Update PrintedDesignSeeder.php

--- database/seeders/PrintedDesignSeeder.php
+++ database/seeders/PrintedDesignSeeder.php
@@ -6,6 +6,7 @@ use Domain\Filaments\Brands\Models\FilamentBrand;
 use Domain\Filaments\Colours\Models\FilamentColour;
 use Domain\Filaments\Materials\Models\FilamentMaterial;
 use Domain\PrintedDesigns\Models\PrintedDesign;
+use Domain\Users\Models\Country;
 use Domain\Users\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -16,15 +17,18 @@ class PrintedDesignSeeder extends Seeder
         $brands = FilamentBrand::all();
         $colours = FilamentColour::all();
         $materials = FilamentMaterial::all();
+        $countries = Country::all();
 
-        for ($i = 0; $i < 30; $i++) {
-            $user = User::factory()->create();
+        for ($i = 0; $i < 2000; $i++) {
+            $user = User::factory()
+                ->for($countries->random())
+                ->create();
             PrintedDesign::factory()
                 ->for($brands->random())
                 ->for($colours->random())
                 ->for($materials->random())
                 ->for($user)
-                ->hasMasterImages()
+                ->hasMasterImages(5)
                 ->hasFavourites(['user_id' => $user->id])
                 ->create();
         }
