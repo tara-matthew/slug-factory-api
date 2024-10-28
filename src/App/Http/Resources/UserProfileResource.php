939c91fe65a95dd3e6dbf7559f0314cb70e03442
tara-matthew <taramatthew.93@gmail.com>
2024-10-28 16:00:11 +0000

    Add Larastan, add properties to UserProfileResource

--- src/App/Http/Resources/UserProfileResource.php
+++ src/App/Http/Resources/UserProfileResource.php
@@ -5,6 +5,11 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @property string $set_public_at
+ * @property string $bio
+ */
+
 class UserProfileResource extends JsonResource
 {
     public function toArray(Request $request): array
