c29b71bd958ab0282f0c492ea0d0ddb4b53df181
tara-matthew <taramatthew.93@gmail.com>
2025-01-22 18:31:53 +0000

    Add notification tests

--- tests/Unit/CreateNotificationActionTest.php
+++ tests/Unit/CreateNotificationActionTest.php
@@ -1,3 +1,46 @@
 <?php
 
-it('creates a notification')->todo();
+use Domain\Notifications\Actions\CreateNotificationAction;
+use Domain\Notifications\DataTransferObjects\NotificationData;
+use Domain\Notifications\Enums\NotificationChannel;
+use Domain\PrintedDesigns\Notifications\PrintedDesignUploaded;
+use Domain\Users\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Notification as NotificationFacade;
+use Tests\TestCase;
+
+uses(RefreshDatabase::class);
+uses(TestCase::class);
+
+it('creates a notification', function () {
+    NotificationFacade::fake();
+
+    $action = new CreateNotificationAction;
+
+    $user = User::factory()->create();
+
+    $notificationData = NotificationData::from([
+        'user' => $user,
+        'title' => 'A notification',
+        'body' => 'A new notification has arrived',
+        'notification_type' => PrintedDesignUploaded::class,
+        'channel' => NotificationChannel::PUSH,
+    ]);
+
+    $this->assertDatabaseCount('notifications', 0);
+
+    $action->execute($notificationData);
+
+    NotificationFacade::assertSentTo($user, PrintedDesignUploaded::class);
+    NotificationFacade::assertCount(1);
+
+    $this->assertDatabaseCount('notifications', 1);
+
+    $this->assertDatabaseHas('notifications', [
+        'user_id' => $user->id,
+        'title' => 'A notification',
+        'body' => 'A new notification has arrived',
+        'channel' => NotificationChannel::PUSH,
+        'read_at' => null,
+    ]);
+});
--- tests/Unit/PrintedDesignUploadedTest.php
+++ tests/Unit/PrintedDesignUploadedTest.php
@@ -1,3 +1,23 @@
 <?php
 
-it('sends a notification when the event is fired')->todo();
+use Domain\PrintedDesigns\Events\PrintedDesignUploaded;
+use Domain\PrintedDesigns\Models\PrintedDesign;
+use Domain\PrintedDesigns\Notifications\PrintedDesignUploaded as PrintedDesignUploadedNotification;
+use Domain\Users\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Notification;
+use Tests\TestCase;
+
+uses(RefreshDatabase::class);
+uses(TestCase::class);
+
+it('sends a notification when the event is fired', function () {
+    Notification::fake();
+
+    $users = User::factory(5)->create();
+    $printedDesign = PrintedDesign::factory()->create();
+
+    PrintedDesignUploaded::dispatch($printedDesign);
+
+    Notification::assertSentTo([$users], PrintedDesignUploadedNotification::class);
+});
