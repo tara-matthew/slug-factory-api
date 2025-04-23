1e193db957d693b12e41e206f4512e4f6f5233b0
tara-matthew <taramatthew.93@gmail.com>
2025-04-23 13:48:22 +0100

    Add seed data for printed design lists

--- database/seeders/PrintedDesignListSeeder.php
+++ database/seeders/PrintedDesignListSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\PrintedDesignLists\Models\PrintedDesignList;
+use Domain\Filaments\Colours\Models\FilamentColour;
+use Domain\PrintedDesigns\Models\PrintedDesign;
 use Domain\Users\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -11,15 +13,22 @@ class PrintedDesignListSeeder extends Seeder
 {
     use WithoutModelEvents;
 
+    public const PRINTED_DESIGNS_PER_LIST = 3;
+
     public function run(): void
     {
         $users = User::all();
+        $prints = PrintedDesign::all();
 
         foreach ($users as $user) {
             foreach (config('default-user-lists') as $listTitle) {
-                PrintedDesignList::factory()->for($user)->create([
+               $list = PrintedDesignList::factory()->for($user)->create([
                     'title' => $listTitle,
                 ]);
+
+               $list->printedDesigns()->attach(
+                    $prints->random(self::PRINTED_DESIGNS_PER_LIST)->pluck('id')
+                );
             }
         }
     }
