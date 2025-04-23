4addab1cf48ddb995a04082c2af302d5eeb6efc7
tara-matthew <taramatthew.93@gmail.com>
2025-04-23 13:48:29 +0100

    Add TODO

--- src/Domain/PrintedDesigns/Actions/UpdatePrintedDesignImagesAction.php
+++ src/Domain/PrintedDesigns/Actions/UpdatePrintedDesignImagesAction.php
@@ -30,6 +30,8 @@ class UpdatePrintedDesignImagesAction
             $item->delete();
         });
 
+        // TODO remove files
+
         $printedDesign->masterImages()->createMany($newImages);
 
         return $printedDesign;
