9dd1a2d048173231e4eaaa793f093e0a851dc3a1
tara-matthew <taramatthew.93@gmail.com>
2025-04-23 16:21:46 +0100

    Rework RemoveFavouriteTest

--- tests/Feature/Favourites/RemoveFavouriteTest.php
+++ tests/Feature/Favourites/RemoveFavouriteTest.php
@@ -1,46 +1,40 @@
 <?php
 
-//
-//use Domain\Favourites\Models\Favourite;
-//use Domain\PrintedDesigns\Models\PrintedDesign;
-//use Domain\Users\Models\User;
-//use Illuminate\Foundation\Testing\RefreshDatabase;
-//use Illuminate\Testing\Fluent\AssertableJson;
-//use Laravel\Sanctum\Sanctum;
-//
-//uses(RefreshDatabase::class);
-//
-//it('deletes a favourite', function () {
-//    $user = User::factory()->create();
-//
-//    $favouritedPrint = PrintedDesign::factory()
-//        ->for($user)
-//        ->create();
-//
-//    Favourite::factory()->for(
-//        $favouritedPrint, 'favouritable'
-//    )->for($user)->create();
-//
-//    $this->assertCount(1, Favourite::all());
-//
-//    Sanctum::actingAs($user);
-//    $this->deleteJson(route('favourites.delete', ['type' => 'printed_design', 'id' => $favouritedPrint->id]))
-//        ->assertNoContent();
-//
-//    $this->assertCount(0, Favourite::all());
-//});
-//
-//it('throws an exception if an attempt is made to unfavourite an item which has not been favourited', function () {
-//    $user = User::factory()->create();
-//
-//    $print = PrintedDesign::factory()
-//        ->for($user)
-//        ->create();
-//
-//    Sanctum::actingAs($user);
-//
-//    $this->deleteJson(route('favourites.delete', ['type' => 'printed_design', 'id' => $print->id]))
-//        ->assertUnprocessable()
-//        ->assertJson(fn (AssertableJson $json) => $json
-//            ->where('message', 'Item has not been added to favourites'));
-//});
+use Domain\Favourites\Models\Favourite;
+use Domain\Filaments\Models\PrinterFilament;
+use Domain\Users\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Laravel\Sanctum\Sanctum;
+
+uses(RefreshDatabase::class);
+
+it('deletes a favourite', function () {
+    $user = User::factory()->create();
+    Sanctum::actingAs($user);
+
+    $favouritedFilament = PrinterFilament::factory()->create();
+
+    Favourite::factory()->for(
+        $favouritedFilament, 'favouritable'
+    )->for($user)->create();
+
+    $this->assertCount(1, Favourite::all());
+
+    $this->deleteJson(route('favourites.delete', ['type' => 'printer_filament', 'id' => $favouritedFilament->id]))
+        ->assertNoContent();
+
+    $this->assertCount(0, Favourite::all());
+});
+
+it('throws an exception if an attempt is made to unfavourite an item which has not been favourited', function () {
+    $user = User::factory()->create();
+    Sanctum::actingAs($user);
+
+    $filament = PrinterFilament::factory()->create();
+
+    $this->deleteJson(route('favourites.delete', ['type' => 'printer_filament', 'id' => $filament->id]))
+        ->assertUnprocessable()
+        ->assertJson(fn (AssertableJson $json) => $json
+            ->where('message', 'Item has not been added to favourites'));
+});
