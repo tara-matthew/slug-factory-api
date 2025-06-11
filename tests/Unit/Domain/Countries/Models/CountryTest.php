8876f2d009308cd0c73e92f13a9025048960fcbb
tara-matthew <taramatthew.93@gmail.com>
2025-06-11 21:55:53 +0100

    Add unit test for Country

---
+++ tests/Unit/Domain/Countries/Models/CountryTest.php
@@ -0,0 +1,13 @@
+<?php
+
+use Domain\Users\Models\Country;
+use Domain\Users\Models\User;
+use Tests\TestCase;
+
+uses(TestCase::class);
+
+test('relations', function () {
+    $country = Country::factory()->hasUsers(2)->create();
+
+    expect($country->users)->each->toBeInstanceOf(User::class );
+});
